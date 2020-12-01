f1a1e437ebaf78370ab1b972447be3798791759e
emfrerichs <70489103+emfrerichs@users.noreply.github.com>
2020-11-30 21:21:15 -0600

    Assign5

---
+++ src/boot/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app"
+
+import "firebase/auth"
+import "firebase/database"
+
+// ADD YOUR FIREBASE CONFIG OBJECT HERE:
+var firebaseConfig = {
+  apiKey: "AIzaSyAG0YbzXun7wL7balITh-eJtdqXHGWG-tA",
+  authDomain: "maverick-5b8f0.firebaseapp.com",
+  databaseURL: "https://maverick-5b8f0.firebaseio.com",
+  projectId: "maverick-5b8f0",
+  storageBucket: "maverick-5b8f0.appspot.com",
+  messagingSenderId: "758045395694",
+  appId: "1:758045395694:web:bdd94462e2a450004401ff"
+}
+
+let firebaseApp = firebase.initializeApp(firebaseConfig)
+let firebaseAuth = firebaseApp.auth()
+let firebaseDb = firebaseApp.database()
+
+export { firebaseAuth, firebaseDb}
